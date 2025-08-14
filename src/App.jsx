8829a7cdd0fe690882df5090e42952225f8e58d6
Abhay-1704 <abhaysps2004@gmail.com>
2025-08-14 21:52:35 +0530

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/header/header.jsx";
+import Footer from "./components/footer/footer.jsx"; // <-- Import Footer
 import "./App.css";
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
             <div className="main-content">
                 <Outlet />
             </div>
+            <Footer />
         </div>
     );
 }
