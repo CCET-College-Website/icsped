72f27c44170baedbc9c4269dd2ac32140c4a099a
Abhay-1704 <abhaysps2004@gmail.com>
2025-08-16 14:15:32 +0530

    Fixed Important Dates Page

--- src/pages/Impdate.jsx
+++ src/pages/Impdate.jsx
@@ -1,35 +1,35 @@
 import React from "react";
-import "./Impdate.css";
+import styles from "./Impdate.module.css";
 
 export default function Impdate() {
   return (
-    <div className="impdate-page">
-      <h1 className="impdate-title">IMPORTANT DATES</h1>
-      <div className="impdate-underline"></div>
-      <table className="impdate-table">
-        <tbody>
+      <div className={styles.impdatePage}>
+        <h1 className={styles.impdateTitle}>IMPORTANT DATES</h1>
+        <div className={styles.impdateUnderline}></div>
+        <table className={styles.impdateTable}>
+          <tbody>
           <tr>
-            <td className="bold">Submission of Full-Length Paper</td>
+            <td className={styles.bold}>Submission of Full-Length Paper</td>
             <td>9th September, 2025</td>
           </tr>
           <tr>
-            <td className="bold">Paper Acceptance Notification</td>
+            <td className={styles.bold}>Paper Acceptance Notification</td>
             <td>9th November, 2025</td>
           </tr>
           <tr>
-            <td className="bold">Submission of Final Version of Paper</td>
+            <td className={styles.bold}>Submission of Final Version of Paper</td>
             <td>30th November, 2025</td>
           </tr>
           <tr>
-            <td className="bold">Registration Deadline (Early Bird)</td>
+            <td className={styles.bold}>Registration Deadline (Early Bird)</td>
             <td>10th December, 2025</td>
           </tr>
           <tr>
-            <td className="bold">Registration Deadline (Late)</td>
+            <td className={styles.bold}>Registration Deadline (Late)</td>
             <td>30th December, 2025</td>
           </tr>
-        </tbody>
-      </table>
-    </div>
+          </tbody>
+        </table>
+      </div>
   );
 }
